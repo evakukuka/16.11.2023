2e31600bb489ecfb82965f7efd3ef05163810cf3
evakukuka <eva.kukuka@grobinasskola.lv>
2023-11-16 15:00:15 +0200

    vvv

---
+++ 4.uzd.py
@@ -0,0 +1,11 @@
+ievadit_skaitli = int(input("Ievadiet skaitli: "))
+
+if ievadit_skaitli < 0:
+    print("Faktoriālu nevar aprēķināt negatīvam skaitlim")
+else: 
+    faktoriāls = 1
+
+for skaitlis in range (1,ievadit_skaitli + 1)
+    faktorials *=skaitlis
+
+    print(f"Faktoriāls no {ievadit_skaitli} ir: {faktorials}")
