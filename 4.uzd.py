dc6a52022cc8fbdf9b0744da8eabae62961f96a4
evakukuka <eva.kukuka@grobinasskola.lv>
2023-11-16 15:01:20 +0200

    vvv

--- 4.uzd.py
+++ 4.uzd.py
@@ -6,6 +6,6 @@ else:
     faktoriāls = 1
 
 for skaitlis in range (1,ievadit_skaitli + 1)
-    faktorials *=skaitlis
+faktorials *=skaitlis
 
     print(f"Faktoriāls no {ievadit_skaitli} ir: {faktorials}")
