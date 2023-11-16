811f00376c367c467aa4cc8d581c73b69528171e
evakukuka <eva.kukuka@grobinasskola.lv>
2023-11-16 14:40:29 +0200

    ......

---
+++ 1.uzdevums.py
@@ -0,0 +1,5 @@
+ievadit_skasitli = int(input("ievadit_skasitli:"))
+summa = 1
+for skaitlis in range (1, ievadit_skasitli +1):
+    summa +=skaitlis
+    print(f"summa no 1 līdz {ievadit_skasitli} ir: {summa}")
